5392237013c60c0dcde8c0c5e8a97b24a8d35e56
Duong <duongminh>
2021-05-29 22:59:54 +0700

    add home, banner component

---
+++ src/components/Search.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './Search.css';
+import 'react-date-range/dist/styles.css';
+// main style
+import 'react-date-range/dist/theme/default.css';
+// theme css file
+import { DateRangePicker } from 'react-date-range';
+import { Button } from '@material-ui/core';
+import PeopleIcon from '@material-ui/icons/People';
+
+// Date picker component
+function Search() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const selectectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: 'selection',
+  };
+
+  function handleSelect(ranges) {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  }
+
+  return (
+    <div className='search'>
+      <DateRangePicker ranges={[selectectionRange]} onChange={handleSelect} />
+      <h2>Number of guests </h2>
+      <PeopleIcon />
+      <input min={0} defaultValue={2} type='number' />
+      <PeopleIcon/>
+      <Button>Search Airbnb</Button>
+    </div>
+  );
+}
+
+export default Search;
